7dd9f176f2829c254501bbf1d324d3af69f10b05
743953 <743953@elf7it218937.ACDM.DS.SAIT.CA>
2018-10-29 21:09:49 -0600

    Week 08 Lab Editing NotesDB.

--- src/java/database/NotesDB.java
+++ src/java/database/NotesDB.java
@@ -14,48 +14,16 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.persistence.EntityManager;
 
 public class NotesDB {
 
     public int insert(Note note) throws NotesDBException {
-        ConnectionPool pool = ConnectionPool.getInstance();
-        Connection connection = pool.getConnection();
-
-        try {
-            String preparedQ = "insert into notes(datecreated,content) values(?,?)";
-            PreparedStatement ps = connection.prepareStatement(preparedQ);
-            ps.setDate(1,new java.sql.Date(note.getDateCreated().getTime()));
-            ps.setString(2, note.getContent());
-            int rows = ps.executeUpdate();
-            return rows;
-        } catch (SQLException ex) {
-            Logger.getLogger(NotesDB.class.getName()).log(Level.SEVERE, "Cannot insert " + note.toString(), ex);
-            throw new NotesDBException("Error inserting note");
-        } finally {
-            pool.freeConnection(connection);
-        }
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
     }
 
     public int update(Note note) throws NotesDBException {
-        ConnectionPool pool = ConnectionPool.getInstance();
-        Connection connection = pool.getConnection();
-
-        try {
-            String preparedSQL = "UPDATE notes SET content = ? WHERE noteId = ?";
-
-            PreparedStatement ps = connection.prepareStatement(preparedSQL);
-
-            ps.setString(1, String.format("%s",note.getContent()));
-            ps.setString(2, String.format("%s",note.getNoteId()));
-
-            int rows = ps.executeUpdate();
-            return rows;
-        } catch (SQLException ex) {
-            Logger.getLogger(NotesDB.class.getName()).log(Level.SEVERE, "Cannot update " + note.toString(), ex);
-            throw new NotesDBException("Error updating note");
-        } finally {
-            pool.freeConnection(connection);
-        }
+        
     }
 
     public List<Note> getAll() throws NotesDBException, ParseException {
